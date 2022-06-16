8dad1824a15dec78190788f156be5677b1d77c54
a.triasoruk <msquinn2017@gmail.com>
2022-06-16 13:19:19 +0300

    tasksList and TaskTile creation

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'widgets/tasks_list.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: 30.0,
+              top: 60.0,
+              right: 30.0,
+              bottom: 30.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                Text(
+                  'to-do list:',
+                  style: TextStyle(
+                    fontFamily: 'Yesteryear-Regular',
+                    decoration: TextDecoration.underline,
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w100,
+                  ),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0,),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: TasksList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
